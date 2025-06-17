77c65b4ce65a87db8ed39deca35fa4142ffb5825
p1erhs <piergiannis@hotmail.com>
2025-06-17 11:32:52 +0300

    change skills

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -4,16 +4,10 @@ export interface Skill {
 }
 
 export const skills: Skill[] = [
-    { name: "Python", iconPath: "/icons/python.svg" },
-    { name: "JavaScript", iconPath: "/icons/js.svg" },
-    { name: "TypeScript", iconPath: "/icons/ts.svg" },
     { name: "HTML5", iconPath: "/icons/html5.svg" },
     { name: "CSS3", iconPath: "/icons/css3.svg" },
-    { name: "React.js", iconPath: "/icons/react.svg" },
+    { name: "JavaScript", iconPath: "/icons/js.svg" },
+    { name: "Python", iconPath: "/icons/python.svg" },
     { name: "Node.js", iconPath: "/icons/nodejs.svg" },
-    { name: "MySQL", iconPath: "/icons/mysql.svg" },
     { name: "SQL", iconPath: "/icons/sql.svg" },
-    { name: "Docker", iconPath: "/icons/docker.svg" },
-    { name: "C/C++", iconPath: "/icons/cpp.svg" },
-    { name: "Java", iconPath: "/icons/java.svg" },
 ];
